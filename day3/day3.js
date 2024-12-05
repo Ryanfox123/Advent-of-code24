7c72308054084286ae1ff6de4964c4b7e690fb63
Ryan Fox <ryanfox1@hotmail.co.uk>
2024-12-05 20:50:41 +0000

    day 3 task 2

--- day3/day3.js
+++ day3/day3.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = __importDefault(require("fs"));
 const text = fs_1.default.readFileSync("./day3.text", "utf8");
+const testText = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
 const findMatches = (data) => {
     const regex = /mul\([0-9]+,[0-9]+\)/g;
     const matches = data.match(regex);
@@ -19,3 +20,23 @@ const findMatches = (data) => {
     console.log(total);
 };
 findMatches(text);
+const findMatchesWhenTold = (data) => {
+    const splitData = data.split("do()");
+    const allowedMemory = [];
+    for (let i = 0; i < splitData.length; i++) {
+        allowedMemory.push(splitData[i].split("don't()")[0]);
+    }
+    console.log(allowedMemory);
+    const regex = /mul\([0-9]+,[0-9]+\)/g;
+    const matches = allowedMemory.join("").match(regex);
+    let total = 0;
+    matches === null || matches === void 0 ? void 0 : matches.forEach((mul) => {
+        const numRegex = /[0-9]+/g;
+        const numMatches = mul.match(numRegex);
+        if (numMatches === null || numMatches === void 0 ? void 0 : numMatches.length) {
+            total += +numMatches[0] * +numMatches[1];
+        }
+    });
+    console.log(total);
+};
+findMatchesWhenTold(text);
--- day3/day3.ts
+++ day3/day3.ts
@@ -1,9 +1,32 @@
 import fs from "fs";
 const text = fs.readFileSync("./day3.text", "utf8");
 
+const testText =
+  "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+
 const findMatches = (data: string) => {
   const regex = /mul\([0-9]+,[0-9]+\)/g;
   const matches = data.match(regex);
+  let total = 0;
+  matches?.forEach((mul) => {
+    const numMatches = mul.match(/[0-9]+/g);
+    if (numMatches?.length) {
+      total += +numMatches[0] * +numMatches[1];
+    }
+  });
+  console.log(total);
+};
+
+findMatches(text);
+
+const findMatchesWhenTold = (data: string) => {
+  const splitData = data.split("do()");
+  const allowedMemory = [];
+  for (let i = 0; i < splitData.length; i++) {
+    allowedMemory.push(splitData[i].split("don't()")[0]);
+  }
+  const regex = /mul\([0-9]+,[0-9]+\)/g;
+  const matches = allowedMemory.join("").match(regex);
   let total = 0;
   matches?.forEach((mul) => {
     const numRegex = /[0-9]+/g;
@@ -15,4 +38,4 @@ const findMatches = (data: string) => {
   console.log(total);
 };
 
-findMatches(text);
+findMatchesWhenTold(text);
